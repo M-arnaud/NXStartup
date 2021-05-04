42dea2ea10389e5859c32700e0e11296e3df11a9
Arnaud <arnaud.monvoisin@janus-engineering.fr>
2021-05-04 09:27:08 +0200

    update
    Signed-off-by: Arnaud <arnaud.monvoisin@janus-engineering.fr>

--- nx_janus.bat
+++ nx_janus.bat
@@ -1,12 +1,12 @@
 set KUNDEN_PP_DIR=H:\Customer
 set CUSTOMER_PP_DIR=%KUNDEN_PP_DIR%
-set JE_BASE_RELEASE_VERSION=v12
-set JE_RELEASE_VERSION=v12
-set CustomerName=Janus_Dev_FR_NX12
+set JE_BASE_RELEASE_VERSION=v1872
+set JE_RELEASE_VERSION=v1872
+set CustomerName=_default
 set UGII_CAM_POST_CONFIGURATOR_INSPECT_TOOL_PREVIEW=1
 set UGII_CAM_POST_CONFIGURATOR_UF6SbNmFujeENr4CuZZ=nx_post_configurator_full
 
-set CUSTOMER_LIST_DIR=H:\Products\NXStartup\
+set CUSTOMER_LIST_DIR=D:\Sources\Machinekits\Products\NXStartup\
  
 echo CUSTOMER_LIST_DIR: %CUSTOMER_LIST_DIR%
  
@@ -14,10 +14,10 @@ echo UGII_BASE_DIR  : %UGII_BASE_DIR%
 echo UGII_ROOT_DIR  : %UGII_ROOT_DIR%
 echo UGII_VERSION   : %UGII_VERSION%
  
-DATE /T > H:\Products\NXStartup\NXStartup.log
-TIME /T >> H:\Products\NXStartup\NXStartup.log
-echo CUSTOMER %CUSTOMER% >> H:\Products\NXStartup\NXStartup.log 
-H:\Products\NXStartup\startup\modify.men >> H:\Products\NXStartup\NXStartup.log
+DATE /T > D:\Sources\Machinekits\Products\NXStartup\NXStartup.log
+TIME /T >> D:\Sources\Machinekits\Products\NXStartup\NXStartup.log
+echo CUSTOMER %CUSTOMER% >> D:\Sources\Machinekits\Products\NXStartup\NXStartup.log 
+D:\Sources\Machinekits\Products\NXStartup\startup\modify.men >> D:\Sources\Machinekits\Products\NXStartup\NXStartup.log
  
 set JE_CUSTOMER_PATH=%CUSTOMER_PP_DIR%\%JE_BASE_RELEASE_VERSION%\%CUSTOMER%
 set JE_LICENSE_FILE_JEL=%JE_CUSTOMER_PATH%\admin\je_license.jel
@@ -29,11 +29,11 @@ echo MsgBox "Kunde nicht vorhanden %JE_CUSTOMER_PATH%", 16, "JANUS NX-Startup" >
 GOTO MESSAGE
  
 :DO
-IF 12 LEQ 4 (
+IF 1872 LEQ 4 (
   set UGII_LICENSE_FILE=27000@%COMPUTERNAME%
-echo UGII_LICENSE_FILE %UGII_LICENSE_FILE% >> H:\Products\NXStartup\NXStartup.log 
+echo UGII_LICENSE_FILE %UGII_LICENSE_FILE% >> D:\Sources\Machinekits\Products\NXStartup\NXStartup.log 
 ) ELSE (
-echo SPLM_LICENSE_SERVER %SPLM_LICENSE_SERVER% >> H:\Products\NXStartup\NXStartup.log 
+echo SPLM_LICENSE_SERVER %SPLM_LICENSE_SERVER% >> D:\Sources\Machinekits\Products\NXStartup\NXStartup.log 
 )
  
 set UGII_ENV_FILE=%CUSTOMER_PP_DIR%\%JE_BASE_RELEASE_VERSION%\_default\ugii_env.dat
@@ -48,7 +48,7 @@ IF EXIST %JE_LICENSE_FILE_JEL% (
   )
 )
  
-echo JE_LICENSE_FILE %JE_LICENSE_FILE% >> H:\Products\NXStartup\NXStartup.log
+echo JE_LICENSE_FILE %JE_LICENSE_FILE% >> D:\Sources\Machinekits\Products\NXStartup\NXStartup.log
 GOTO ENDE
  
 :MESSAGE
--- nx_universal.bat
+++ nx_universal.bat
@@ -1,28 +1,32 @@
 @echo off
-set CUSTOMER=Janus_Dev_FR_NX12
-set CustomerName=Janus_Dev_FR_NX12
+set CUSTOMER=_default
+set CustomerName=_default
 set UGII_CAM_POST_CONFIGURATOR_INSPECT_TOOL_PREVIEW=1
 set UGII_CAM_POST_CONFIGURATOR_UF6SbNmFujeENr4CuZZ=nx_post_configurator_full
 set UGII_LANG=english
 set UGII_DISPLAY_DEBUG=0
-set UGII_BASE_DIR=D:\Program Files\Siemens\NX12.0.2.MP08_64Bit\
-set UGII_ROOT_DIR=D:\Program Files\Siemens\NX12.0.2.MP08_64Bit\UGII\
-set UGII_UGMANAGER_DIR=D:\Program Files\Siemens\NX12.0.2.MP08_64Bit\NXBIN\
+set UGII_BASE_DIR=D:\Program Files\Siemens\NX1872_64Bit\
+set UGII_ROOT_DIR=D:\Program Files\Siemens\NX1872_64Bit\UGII\
+set UGII_UGMANAGER_DIR=D:\Program Files\Siemens\NX1872_64Bit\NXBIN\
 set UGII_VERSION_BIT=64Bit
 
 set JE_POST_POOL_DIR=P:\PP\Pool\
-set JE_POST_POOL_DIR_NG=D:\sources\MachineKits\NGMK\NGMK_Pool\
-set JE_POOL_NGMK_CONTENT=D:\Sources\Machinekits\NGMK\NGMK_Content\
+set JE_POST_POOL_DIR_NG=P:\NGMK\NGMK_Pool\
+set JE_POOL_NGMK_CONTENT=P:\NGMK\NGMK_Content\
 
 echo CUSTOMER : %CUSTOMER%
 echo LANGUAGE : english
-set JE_NX_VERSION=NX12.0.2.MP08_64BIT
-set JE_BACKGROUND_URL=H:\Products\NXStartup\background\STGBackground.html
-call H:\Products\NXStartup\nx_janus.bat
+set JE_NX_VERSION=NX1872_64BIT
+set JE_BACKGROUND_URL=D:\Sources\Machinekits\Products\NXStartup\background\STGBackground.html
+call D:\Sources\Machinekits\Products\NXStartup\nx_janus.bat
 
 
-subst w: /d
-subst w: %JE_CUSTOMER_PATH%/custom/library/machine/installed_machines
+subst z: /d
+subst z: %JE_CUSTOMER_PATH%/custom/library/machine/installed_machines
+
+
+subst y: /d
+subst y: %JE_CUSTOMER_PATH%
 
 
 "%UGII_ROOT_DIR%\ugraf" %1 %2 %3 %4 %5 %6 %7 %8 %9
